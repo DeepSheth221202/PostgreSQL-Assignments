7791cebaff7af983fba297e303ea1790ad726c8e
DeepSheth221202 <deep.sheth@etatvasoft.com>
2024-01-29 15:09:17 +0530

    Assignment 3 done

--- Assignment 2.sql
+++ Assignment 2.sql
@@ -1,3 +1,5 @@
+/* the "actor" table has names of all actors. Can you tell me which actor first names are most common and how many records have it? */
+
 SELECT first_name , COUNT(first_name) 
 FROM actor
 GROUP BY first_name
---
+++ Assignment 3.sql
@@ -0,0 +1,61 @@
+/*I want something to analyse the pattern between film lang and its rental rate Send me as data extract (report) that shows lang of each movie , 
+title , and rental rate*/
+  
+ SELECT f.title , l.name as "Movie Language" , f.rental_rate
+ FROM film f
+ JOIN language l
+ ON f.language_id = l.language_id;
+ 
+ 
+ /* Show me a list of actors and the count of movies they have acted in 
+ .Sort it in a way that the actor who hase done most movies at top*/
+ 
+ 
+ SELECT CONCAT(a.first_name , ' ' ,a.last_name) as "actor name" , COUNT(fa.film_id)
+ FROM actor a
+ JOIN film_actor fa
+ ON a.actor_id = fa.actor_id
+ GROUP BY a.actor_id
+ ORDER BY COUNT(fa.film_id) DESC;
+ 
+ 
+ /*
+ report that shows the diff ratings of 
+ all movies and count of movies that have been rented for each rating
+ */
+ 
+ 
+--  select * from film -- film_id , rating
+--  select * from rental -- rental_id , inventory_id , customer_id
+--  select * from inventory -- inventory_id , film_id
+ 
+ SELECT f.rating , COUNT(i.inventory_id) 
+ FROM inventory i
+ RIGHT JOIN film f ON f.film_id = i.film_id
+ JOIN rental r ON r.inventory_id = i.inventory_id
+ GROUP BY f.rating
+ ORDER BY COUNT(i.inventory_id) DESC;
+ 
+ 
+ /*The rental table has fields from rental_date and return date .
+ We want to reach out customers whose rental duration eas 7 or more days and 
+ send them an email.create the report that list down following cols:
+ -Rental date
+ -Return date
+ -Rent duration
+ -Customer First name
+ -Customer Last Name
+ -Customer Email
+ 
+ */
+ 
+ 
+ SELECT r.rental_date , r.return_date , AGE(r.return_date , r.rental_date) AS "rental duration" , c.first_name, c.last_name , c.email
+ FROM rental r 
+ JOIN customer c ON r.customer_id = c.customer_id
+ WHERE r.return_date IS NOT NULL AND AGE(r.return_date , r.rental_date) > INTERVAL '7 days'
+ ORDER BY "rental duration" DESC;
+ 
+ 
+ 
+ 
